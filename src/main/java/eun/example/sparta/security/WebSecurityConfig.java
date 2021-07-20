614b47f42a96735c186480bb8c0d160042b8f8bb
HongEunbeen <vvvv980@gmail.com>
2021-07-20 21:39:22 +0900

    feat : 패스워드 암호화 구현

--- src/main/java/eun/example/sparta/security/WebSecurityConfig.java
+++ src/main/java/eun/example/sparta/security/WebSecurityConfig.java
@@ -1,13 +1,23 @@
 package eun.example.sparta.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+        //객체를 선언만 해주면 된다.
+        //다른 곳에서 사용할 수 있도록 Bean으로 등록
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
--- src/main/java/eun/example/sparta/service/UserService.java
+++ src/main/java/eun/example/sparta/service/UserService.java
@@ -5,31 +5,34 @@ import eun.example.sparta.model.User;
 import eun.example.sparta.model.UserRole;
 import eun.example.sparta.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class UserService {
-
+    private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private static final String ADMIN_TOKEN = "AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC";
 
     @Autowired
-    public UserService(UserRepository userRepository) {
-        // 멤버 변수 생성
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public void registerUser(SignupRequestDto requestDto){
+
+    public void registerUser(SignupRequestDto requestDto) {
         String username = requestDto.getUsername();
-        String password = requestDto.getPassword();
         // 회원 ID 중복 확인
         Optional<User> found = userRepository.findByUsername(username);
         if (found.isPresent()) {
             throw new IllegalArgumentException("중복된 사용자 ID 가 존재합니다.");
         }
 
+        // 패스워드 인코딩
+        String password = passwordEncoder.encode(requestDto.getPassword());
         String email = requestDto.getEmail();
         // 사용자 ROLE 확인
         UserRole role = UserRole.USER;
@@ -43,5 +46,4 @@ public class UserService {
         User user = new User(username, password, email, role);
         userRepository.save(user);
     }
-
 }
