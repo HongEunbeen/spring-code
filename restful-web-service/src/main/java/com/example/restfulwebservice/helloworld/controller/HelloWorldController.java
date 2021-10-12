6ad2f67811d4058e51afce23438601970253cdd0
HongEunbeen <vvvv980@gmail.com>
2021-10-12 14:54:45 +0900

    feat : user 도메인 클래스 생성

--- restful-web-service/src/main/java/com/example/restfulwebservice/helloworld/controller/HelloWorldController.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/helloworld/controller/HelloWorldController.java
@@ -1,7 +1,8 @@
-package com.example.restfulwebservice.controller;
+package com.example.restfulwebservice.helloworld.controller;
 
-import com.example.restfulwebservice.HelloWorldBean;
+import com.example.restfulwebservice.helloworld.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +18,11 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean(){
         return new HelloWorldBean("Hello World");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
