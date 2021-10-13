02c3eff2093b345e3aa41685271d2c984d08c71d
HongEunbeen <vvvv980@gmail.com>
2021-10-13 16:42:06 +0900

    feat : api filtering

---
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/AdminUserController.java
@@ -0,0 +1,60 @@
+package com.example.restfulwebservice.user;
+
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminUserController {
+    private UserDaoService service;
+
+    public AdminUserController(UserDaoService service){
+        this.service = service;
+    }
+
+    //사용자 전체목록 조희
+    @GetMapping("/users")
+    public MappingJacksonValue retrieveAllUsers(){
+        List<User> users = service.findAll();
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id,", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(users);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+
+    //사용자 한 명 조회
+    //GET /users/1 -> 서버측에는 문자 형태(String)으로 옴 -> int로 자동 변환된다.
+    @GetMapping("/users/{id}")
+    public MappingJacksonValue retrieveUser(@PathVariable int id){
+        User user = service.findOne(id);
+        if(user == null) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+
+        SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter
+                .filterOutAllExcept("id,", "name", "joinDate", "ssn");
+
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserInfo", filter);
+
+        MappingJacksonValue mapping = new MappingJacksonValue(user);
+        mapping.setFilters(filters);
+
+        return mapping;
+    }
+}
