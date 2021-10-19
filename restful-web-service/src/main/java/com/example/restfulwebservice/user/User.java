1dbad877e7029763571a68a9c2a3d036e2e6b443
HongEunbeen <vvvv980@gmail.com>
2021-10-19 18:11:27 +0900

    feat : jpa 적용 API 작성

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,17 +9,26 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 //@JsonIgnoreProperties(value={"password","ssn"})
 //@JsonFilter("UserInfo")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message ="Name 2글자 이상 입력해 주세요.")
@@ -36,4 +45,15 @@ public class User {
 
     @ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요.")
     private String ssn;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
+
+    public User(Integer id, String name, Date joinDate, String password, String ssn) {
+        this.id = id;
+        this.name = name;
+        this.joinDate = joinDate;
+        this.password = password;
+        this.ssn = ssn;
+    }
 }
---
+++ restful-web-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into user values(101, sysdate(), 'User1', 'test111', '701010-111111');
+insert into user values(102, sysdate(), 'User2', 'test222', '801010-111111');
+insert into user values(103, sysdate(), 'User3', 'test333', '901010-111111');
+
+
+insert into post values(1001, 'My first post', 101);
+insert into post values(1002, 'My second post', 101);
